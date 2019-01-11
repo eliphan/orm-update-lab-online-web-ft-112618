e91f4b8dff0d022803cafa1fc3f3df4b220fa0ed
Elise Phan <aliceconnic@gmail.com>
2019-01-11 03:52:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,6 +14,11 @@ attr_reader :id
 
   def self.create_table
     sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade NUMERIC
+      )
     SQL
     DB[:conn].execute(sql,)
   
