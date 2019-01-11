15a4f393e4d6e4628f658e81cb10a7f7e781d555
Elise Phan <aliceconnic@gmail.com>
2019-01-11 04:18:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,7 +48,7 @@ attr_reader :id
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
   
-  def self.create
+  def self.create(name:, grade:)
     student = self.new(name,grade)
     student.save
     student
