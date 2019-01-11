f3bddf902d754625ded024ee550adcd0cf668f26
Elise Phan <aliceconnic@gmail.com>
2019-01-11 03:57:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,5 +29,16 @@ attr_reader :id
     SQL
     DB[:conn].execute(sql)
   end
+  
+  def save
+    if self.id
+       self.update
+    else
+      sql = <<-SQL
+      SQL
+      DB[:conn].execute(sql,self.name,self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
 
 end
