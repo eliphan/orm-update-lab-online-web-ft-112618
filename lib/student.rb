d012af473d4261a379c8d1f8944f545f585f8ed6
Elise Phan <aliceconnic@gmail.com>
2019-01-11 04:00:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -44,7 +44,7 @@ attr_reader :id
   end
   
   def update
-    sql = "UPDATE students SET name = ?, album = ? WHERE id = ?"
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
     DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 
